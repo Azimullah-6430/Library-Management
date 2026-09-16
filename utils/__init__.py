3d476373c9234eb73eb631d2b721bd6bf869ddbf
AZIMULLAH A <azim44667@gmail.com>
2026-09-16 23:39:10 +0530

    Refactor utils/__init__.py with imports and __all__
    Updated the __init__.py file to include imports and define __all__ for the utility package.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,16 @@
-"""
-Utility package for the Library Book Management System.
-"""
+# utils/__init__.py
+
+from .book_manager import book_manager
+from .book_scanner import scan_book_images, scan_book
+from .excel_manager import excel_manager
+from .image_manager import image_manager
+from .ocr_manager import OCRManager
+
+__all__ = [
+    "book_manager",
+    "scan_book_images",
+    "scan_book",
+    "excel_manager",
+    "image_manager",
+    "OCRManager",
+]
